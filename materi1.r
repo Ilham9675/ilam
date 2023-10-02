b8c4f8dffd74d554ce30a76deb60cc98de47607a
Ilham9675 <ilhamakbar9675@gmail.com>
2023-10-02 18:50:38 +0700

    data ml

---
+++ materi1.r
@@ -0,0 +1,21 @@
+# 1 operator penugasan / penjelasan => =,<-,->
+x = 20
+y <- 40
+50 -> z
+# 2 operator aritmatika => +,-,*,/,^
+
+hasil1 = x + y
+hasil2 = x - z
+hasil3 = x * y
+hasil4 = x / y
+
+hasil5 = x ^ 2
+
+
+
+# 3 operator pembanding => >, <,>=,<=,==,!=
+
+
+
+
+# 4 operator logika => &&,||,!
---
+++ meteri2.r
@@ -0,0 +1,13 @@
+nilai1 = 40
+nilai2 = 100
+if(nilai1 > 40 || nilai2 == 100){
+    print(nilai)
+}else{
+    print("lebih kecil sam dengan 40")
+}
+
+angka = "satu"
+
+switch(angka,
+    "satu" = {print("satu")}
+)
